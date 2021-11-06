d7a5a5f147c6558022181bf66d433b0faa63f90d
Daniel <danielmtran.dev@gmail.com>
2021-11-06 18:51:28 -0400

    simple-insta

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+	url: 'mongodb+srv://Daniel:SzHInKisNNrW1sfq@cluster0.bbitw.mongodb.net/GardnerGram?retryWrites=true&w=majority',
+	'dbName': 'GardnerGram',
+};
